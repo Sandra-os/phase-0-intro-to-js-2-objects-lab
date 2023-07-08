69cf38ecc15263056d38e341aace0d99bc587fee
sandra ogbudinkpa <sandra.os325@gmail.com>
2023-07-07 20:19:52 -0400

    creates a new object with the same properties

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Write your solution in this file!
+// Initialize the employee object
+let employee = {
+    name: 'Employee Name',
+    streetAddress: '123 Street'
+};
+
+// Functions
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {...employee, [key]: value};
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    let newObj = {...employee};
+    delete newObj[key];
+    return newObj;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
